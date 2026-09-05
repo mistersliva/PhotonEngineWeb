c59383cefa49d4159de17c108a215329e2ec7886
mistersliva <inloxtt2021@gmail.com>
2026-09-05 14:18:14 +0200

    Fix WASM: use spawn_app instead of run_app, remove WebGL context theft from JS

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use photon_engine::app::App;
 
 fn main() {
     #[cfg(target_arch = "wasm32")]
-    console_error_panic_hook::set_once();
+    {
+        console_error_panic_hook::set_once();
+        web_sys::console::log_1(&"PhotonEngine: main() started".into());
+    }
 
     #[cfg(not(target_arch = "wasm32"))]
     {
@@ -15,11 +18,11 @@ fn main() {
 
     #[cfg(target_arch = "wasm32")]
     {
-        wasm_bindgen_futures::spawn_local(async {
-            let event_loop = winit::event_loop::EventLoop::new().unwrap();
-            let mut app = App::new();
-            event_loop.run_app(&mut app).expect("Event loop error");
-        });
+        let event_loop = winit::event_loop::EventLoop::new().unwrap();
+        web_sys::console::log_1(&"PhotonEngine: EventLoop created, calling spawn_app".into());
+        use winit::platform::web::EventLoopExtWebSys;
+        let _ = event_loop.spawn_app(App::new());
+        web_sys::console::log_1(&"PhotonEngine: spawn_app returned".into());
     }
 
     #[cfg(not(target_arch = "wasm32"))]
